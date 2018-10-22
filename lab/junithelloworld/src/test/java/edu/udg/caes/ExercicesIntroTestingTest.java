84ba8fd3dc2cbc3b4be5227e99e6e79f724e8f6d
magalenyo <magalenyo7@gmail.com>
2018-10-22 09:47:41 +0200

    4 tests nous. Miguel Ángel Bueno Rivera

--- lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
+++ lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
@@ -3,13 +3,20 @@ package edu.udg.caes;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ExercicesIntroTestingTest {
 
     @Test
     public void testFindLast(){
         final int result = ExercicesIntroTesting.findLast(new int []{2, 3, 5}, 2);
-        assertEquals(result, 0);
+         assertEquals(result, 0);
+    }
+
+    @Test
+    public void testFindLastEmpty(){
+        final int result = ExercicesIntroTesting.findLast(new int []{}, 2);
+        assertEquals(result, -1);
     }
 
     @Test
@@ -19,14 +26,32 @@ public class ExercicesIntroTestingTest {
     }
 
     @Test
+    public void testLastZeroTwoElements(){
+        final int result = ExercicesIntroTesting.lastZero(new int []{0, 1});
+        assertEquals(result, 0);
+    }
+
+    @Test
     public void testCountPositive(){
         final int result = ExercicesIntroTesting.countPositive(new int []{-4, 2, 0, 2});
         assertEquals(result, 2);
     }
 
     @Test
+    public void testCountPositiveNullArray(){
+        assertThrows(NullPointerException.class,
+                () -> ExercicesIntroTesting.countPositive(null),"Array nula");
+    }
+
+    @Test
     public void testOddOrPos(){
         final int result = ExercicesIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
         assertEquals(result, 3);
+    }
+
+    @Test
+    public void testOddOrPosOneElement(){
+        final int result = ExercicesIntroTesting.oddOrPos(new int []{1});
+        assertEquals(result, 1);
     }
 }
